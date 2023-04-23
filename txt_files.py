1073f1b3f451f78944dd493b920985e7078cc16a
Niiikogda <niikogda@gmail.com>
2023-04-23 10:48:14 +0300

    done_2

--- txt_files.py
+++ txt_files.py
@@ -1,1 +1,21 @@
-
+try:
+    flag = False
+    words=input("what do you wanna find? :")
+    words+='\n'
+    with open("user_finder.txt", "r") as file:
+#    myfile = open("user_finder.txt", "w")
+#    x=input("enter something pls: ")
+#    res=myfile.write(x)
+        for line in file: 
+            print(line)
+            if words == line:
+                flag = True
+                break
+        if flag == True:
+           print("line exist")
+        else:
+           print("line doesn`t exist")
+except Exception as e:
+        print(e)
+finally:
+        file.close()
