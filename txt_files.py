c5992cc14a106bef1478d8409dfd0459cefb2e89
Laptop <Laptop@DESKTOP-BVI07RL>
2023-04-29 19:52:05 +0300

    done

--- txt_files.py
+++ txt_files.py
@@ -1,1 +1,10 @@
+def sort_file():
+    file_name = input("Enter file name: ")
+    with open(file_name, 'r') as file:
+        content = file.readlines()
+        content.sort()
+    with open(file_name, 'w') as file:
+        file.writelines(content)
+
+sort_file()
 
