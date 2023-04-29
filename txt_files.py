8699384d947bb65001bcd70f7901f2ce1f379037
Laptop <Laptop@DESKTOP-BVI07RL>
2023-04-29 19:47:08 +0300

    done

--- txt_files.py
+++ txt_files.py
@@ -1,1 +1,14 @@
+def print_last_n_lines():
+    file_name = input("Enter file name: ")
+    n = int(input("Enter number of lines to print: "))
+    with open(file_name, 'r') as file:
+        stack = []
+        for line in file:
+            stack.append(line.strip())
+            if len(stack) > n:
+                stack.pop(0)
+        for line in stack:
+            print(line)
+
+print_last_n_lines()
 
