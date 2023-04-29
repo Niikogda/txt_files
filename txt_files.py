47f80321c28fb8496c7f01cd67e24f51525012f5
Laptop <Laptop@DESKTOP-BVI07RL>
2023-04-29 19:50:22 +0300

    done

--- txt_files.py
+++ txt_files.py
@@ -1,1 +1,12 @@
+def check_duplicates():
+    file_name = input("Enter file name: ")
+    with open(file_name, 'r') as file:
+        content = file.readlines()
+        unique_content = set(content)
+        if len(content) == len(unique_content):
+            print("No duplicate lines found in the file")
+        else:
+            print("Duplicate lines found in the file")
+
+check_duplicates()
 
