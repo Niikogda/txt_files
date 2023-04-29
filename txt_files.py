5e2732aee38b7d9df41eea18b65bec5322786d34
Laptop <Laptop@DESKTOP-BVI07RL>
2023-04-29 19:48:34 +0300

    done

--- txt_files.py
+++ txt_files.py
@@ -1,1 +1,12 @@
+def replace_line():
+    file_name = input("Enter file name: ")
+    old_line = input("Enter the line to replace: ")
+    new_line = input("Enter the new line: ")
+    with open(file_name, 'r+') as file:
+        content = file.read()
+        file.seek(0)
+        new_content = content.replace(old_line, new_line)
+        file.write(new_content)
+
+replace_line()
 
