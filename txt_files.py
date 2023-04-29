02473b13ae58d033a48368f3506d7fef3209b3b0
Laptop <Laptop@DESKTOP-BVI07RL>
2023-04-29 19:53:20 +0300

    done

--- txt_files.py
+++ txt_files.py
@@ -1,1 +1,12 @@
+def delete_line():
+    file_name = input("Enter file name: ")
+    line_to_delete = input("Enter the line to delete: ")
+    with open(file_name, 'r') as file:
+        content = file.readlines()
+    with open(file_name, 'w') as file:
+        for line in content:
+            if line.strip() != line_to_delete:
+                file.write(line)
+
+delete_line()
 
